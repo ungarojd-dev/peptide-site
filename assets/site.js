4b3c689e6c1c4c5cdc36d8d688e2ef4f5ba3c5e6
ungarojd-dev <ungarojd@gmail.com>
2026-07-29 22:08:15 -0400

    hero shrink

--- assets/site.js
+++ assets/site.js
@@ -198,7 +198,7 @@
     update();
   }
 
-  const PROMOTIONS_URL="/data/promotions.json?v=20260730-instant-sale-v8";
+  const PROMOTIONS_URL="/data/promotions.json?v=20260730-compact-deals-v9";
   const promoState={all:[],active:[],loaded:false};
   const promotionTime=value=>value?new Date(value).getTime():null;
   const isPromotionActive=(promotion,when=Date.now())=>{
