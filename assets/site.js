2211f091760c34de5b98ab0876661fc5a4f1177d
ungarojd-dev <ungarojd@gmail.com>
2026-08-25 14:06:30 -0400

    aurora 20

--- assets/site.js
+++ assets/site.js
@@ -233,7 +233,7 @@
     update();
   }
 
-  const PROMOTIONS_URL="/data/promotions.json?v=20260822-popup-cooldown-v76";
+  const PROMOTIONS_URL="/data/promotions.json?v=20260825-aurora-code20-v77";
   const promoState={all:[],active:[],loaded:false};
   const promotionTime=value=>value?new Date(value).getTime():null;
   const isPromotionActive=(promotion,when=Date.now())=>{
